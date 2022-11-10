b9f4e0eed905ece76cd16e03f4e2fc27c605e2cb
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2022-11-10 11:05:39 +0600

    Admin Controller

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    public function __construct()
+    {
+        /*
+         * Uncomment the line below if you want to use verified middleware
+         */
+        //$this->middleware('verified:admin.verification.notice');
+    }
+
+
+    public function index(){
+        return view('admin.dashboard');
+    }
+}
