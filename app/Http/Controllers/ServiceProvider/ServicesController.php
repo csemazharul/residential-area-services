11deec4a596c256ec8b1cbb4c0d6816baeeb2c55
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2022-11-19 17:00:41 +0600

    new controller added

---
+++ app/Http/Controllers/ServiceProvider/ServicesController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\ServiceProvider;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Divisions;
+use Illuminate\Http\Request;
+
+class ServicesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $divisions = Divisions::all();
+        $categories = Category::all();
+        $page_title ="Add Service";
+        return view('service-provider.services.create', compact('divisions', 'categories','page_title'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
