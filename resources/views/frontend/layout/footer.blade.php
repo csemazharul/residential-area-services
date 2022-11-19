60fac936e41504a9c47c31549dc19b79624011d1
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2022-11-19 17:04:04 +0600

    added frontend file

---
+++ resources/views/frontend/layout/footer.blade.php
@@ -0,0 +1,54 @@
+
+    <footer class="footer-area" style="background-image:url(assets/images/footer-bg.png)">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="widget widget_about yellow-color">
+                        <div class="widget-title-area">
+                            <h3 class="widget-title">
+                                About Residential area services
+                            </h3><!-- /.widget-title -->
+                        </div><!-- /.widget-title-area -->
+                        <div class="widget-about-content">
+                            <!-- <img src="{{asset('resources/frontend/assets/images/footer-logo.png')}}" alt="house" /> -->
+                            <p>We Provide Premium Word Press, Ghost and HTML template. Our Perm tritium Templates is, develop gaped in a way so that the clients find  Support. Themes are developed in a way so that the clients find.</p>
+                            <a href="#" class="button">More</a>
+                        </div><!-- /.widget-content -->
+                    </div><!-- /.widget widget_about -->
+                </div><!-- /.col-md-4 -->
+                <div class="col-md-4">
+                    <div class="widget widget_place_category yellow-color">
+                        <div class="widget-title-area">
+                            <h3 class="widget-title">Place Category</h3><!-- /.widget-title -->
+                        </div><!-- /.widget-title-area -->
+                        <ul>
+                            <li>Flat for Rent <a href="#">Francis</a></li>
+                            <li>Flat for Rent <a href="#">Collins St</a></li>
+                            <li>Flat for Rent <a href="#">Rose Ln</a></li>
+                            <li>Flat for Rent <a href="#">Cosgrave Ln</a></li>
+                            <li>Flat for Rent <a href="#">Bourke St</a></li>
+                            <li>Flat for Rent <a href="#">Flienders Ln</a></li>
+                        </ul>
+                    </div><!-- /.widget -->
+                </div><!-- /.col-md-4 -->
+                <div class="col-md-4">
+                    <div class="widget widget_instagram yellow-color">
+                        <div class="widget-title-area">
+                            <h3 class="widget-title">Instagram Image</h3><!-- /.widget-title -->
+                        </div><!-- /.widget-title-area -->
+                        <div class="instagram-image-content">
+
+
+                        </div><!-- /.instagram-image-content -->
+                    </div><!-- /.widget -->
+                </div><!-- /.col-md-4 -->
+            </div><!-- /.row -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="bottom-content">
+                        <p>Copyright  &copy;2022 <a href="#">Residential area services</a></p>
+                    </div><!-- /.bottom-top-content -->
+                </div><!-- /.col-md-12 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </footer><!-- /.footer-area -->
---
+++ resources/views/frontend/layout/layout.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<!-- Mirrored from htmlguru.net/house-rent/index07.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 19 Sep 2022 19:08:59 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+	<!-- Basic Page Needs
+	================================================== -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<!-- Specific Meta
+	================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="description" content="glimmer is a modern presentation HTML5 Blog template.">
+	<meta name="keywords" content="HTML5, Template, Design, Development, Blog" />
+	<meta name="author" content="">
+
+	<!-- Titles
+	================================================== -->
+	<title>House07</title>
+
+	<!-- Favicons
+		================================================== -->
+	<link rel="shortcut icon" href="{{asset('resources/frontend/assetss/images/house-logo.png')}}">
+	<link rel="apple-touch-icon" href="{{asset('resources/frontend/assetss/images/apple-touch-icon.html')}}">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.html')}}">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.html')}}">
+
+	<!-- Custom Font
+	================================================== -->
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i%7cPoppins:300,400,500,600,700" rel="stylesheet">
+    @yield('import-css')
+	<!-- CSS
+	================================================== -->
+	<link rel="stylesheet" href="{{asset('resources/frontend/assetss/css/plugins.css">
+    <link rel="stylesheet" href="{{asset('resources/frontend/assetss/css/colors.css">
+	<link rel="stylesheet" href="style.css">
+	<!-- Modernizr
+    @yield('style')
+	================================================== -->
+	<script src="{{asset('resources/frontend/assets/js/vendor/modernizr-2.8.3.min.js')}}"></script>
+</head>
+
+<body>
+@include('./header')
+@yield('content')
+@include('./footer')
+	<script src="{{asset('resources/frontend/assets/js/vendor/jquery-1.12.4.min.js')}}"></script>
+	<script src="{{asset('resources/frontend/assets/js/plugins.js')}}"></script>
+	<script src="{{asset('resources/frontend/assets/js/main.js')}}"></script> <!-- main-js -->
+
+</body>
+
+<!-- Mirrored from htmlguru.net/house-rent/index07.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 19 Sep 2022 19:08:59 GMT -->
+</html>
