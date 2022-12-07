0b9a2b930f0140158b30e74a12d13699d08f4565
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2022-12-08 02:04:00 +0600

    admin and api route

---
+++ routes/admin.php
@@ -0,0 +1,79 @@
+<?php
+
+use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Admin\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Admin\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Admin\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Admin\Auth\NewPasswordController;
+use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Admin\Auth\RegisteredUserController;
+use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/', [DashboardController::class, 'index'])
+        ->middleware('auth:admin');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->middleware('auth:admin')
+        ->name('dashboard');
+
+    Route::get('/register', [RegisteredUserController::class, 'create'])
+        ->middleware('guest:admin')
+        ->name('register');
+
+    Route::post('/register', [RegisteredUserController::class, 'store'])
+        ->middleware('guest:admin');
+
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+        ->middleware('guest:admin')
+        ->name('login');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+        ->middleware('guest:admin');
+
+    Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
+        ->middleware('guest:admin')
+        ->name('password.request');
+
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->middleware('guest:admin')
+        ->name('password.email');
+
+    Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
+        ->middleware('guest:admin')
+        ->name('password.reset');
+
+    Route::post('/reset-password', [NewPasswordController::class, 'store'])
+        ->middleware('guest:admin')
+        ->name('password.update');
+
+    Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
+        ->middleware('auth:admin')
+        ->name('verification.notice');
+
+    Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+        ->middleware(['auth:admin', 'signed', 'throttle:6,1'])
+        ->name('verification.verify');
+
+    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+        ->middleware(['auth:admin', 'throttle:6,1'])
+        ->name('verification.send');
+
+    Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
+        ->middleware('auth:admin')
+        ->name('password.confirm');
+
+    Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
+        ->middleware('auth:admin');
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->middleware('auth:admin')
+        ->name('logout');
+    Route::resource('categories', CategoryController::class)->middleware('auth:admin');
+
+
+});
