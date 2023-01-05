3cbf1a7e71a4d56268871771425f6979407e28d5
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2023-01-05 22:09:27 +0600

    updated user controller

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Services;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
@@ -12,9 +13,54 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        // $users = User::all();
+        //get service provider list from users
+        // $users = User::whereHas('roles', function ($q) {
+        //     $q->where('name', 'service-provider');
+        // })->get();
 
-        return view('admin.users.index', compact('users'));
+        // return $users;
+        // die;
+
+        // return view('admin.users.index', compact('users'));
+    }
+
+    public function getUsers(){
+        $users = User::whereHas('roles', function ($q) {
+            $q->where('name', 'user');
+        })->get();
+
+        return view('admin.users.list', compact('users'));
+    }
+
+    public function getServiceProviders(){
+        $providers = User::whereHas('roles', function ($q) {
+            $q->where('name', 'service-provider');
+        })->get();
+
+        return view('admin.providers.list', compact('providers'));
+    }
+
+    public function deletedUser($id){
+     
+        $deleted = User::where('id',$id)->delete();
+        if($deleted){
+            return back()->with('success', 'User deleted successfully.');
+        }else{
+            return back()->with('success', 'User not deleted.');
+        }
+    }
+
+    public function blockUser($id, $staus){
+        $status = $staus == 1 ? 0 : 1;
+        $message = $staus == 1 ? 'User blocked successfully.' : 'User unblocked successfully.';
+        $blocked = User::where('id',$id)->update(['status'=>$status]);
+        if($blocked){
+            return back()->with('success', $message);
+        }else{
+            return back()->with('success', 'User not blocked.');
+        }
+
     }
 
     public function show(User $user)
@@ -72,4 +118,6 @@ class UserController extends Controller
 
         return back()->with('message', 'User deleted.');
     }
+
+
 }
