571c0f2a2c157091daea3a2402f08310f12ea816
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2023-01-17 23:39:00 +0600

    Email Notification

---
+++ app/Notifications/EmailNotification.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class EmailNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+
+     protected $booking;
+
+    public function __construct($booking)
+    {
+        $this->booking = $booking;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                     ->greeting($this->booking['title'])
+                    ->line($this->booking['body'])
+                    ->action($this->booking['actionText'], $this->booking['actionURL'])
+                    // ->line('Thank you for using our application!');
+                    ->line($this->booking['thanks']);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
