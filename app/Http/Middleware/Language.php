544fc744144bf0d26c695f3234ed76d91f21ee26
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2023-01-17 23:36:00 +0600

    Language added

---
+++ app/Http/Middleware/Language.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Session;
+
+class Language
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Session::has('locale')) {
+            App::setLocale(Session::get('locale'));
+        }
+        else { // This is optional as Laravel will automatically set the fallback language if there is none specified
+            App::setLocale(Config::get('app.fallback_locale'));
+        }
+        return $next($request);
+    }
+}
